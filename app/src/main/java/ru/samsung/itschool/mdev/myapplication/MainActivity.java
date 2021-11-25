d75f2bc510a4987fd7e90cfb85b94d5e99f71b27
Pavel <impetus@yandex.ru>
2021-11-25 18:53:28 +0300

    Отправка данных из 2-ой активности в первую

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package ru.samsung.itschool.mdev.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,13 +44,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Явное намерение
-                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
-                intent.putExtra("ccc",ed.getText().toString());
-                startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                intent.putExtra("ccc", ed.getText().toString());
+                startActivity(intent); // если мы не ждем обратно ничего
+              //  startActivityForResult(intent, ACTIVITY_RESULT_CODE);
+                someActivityResultLauncher.launch(intent);
             }
         });
-
-
-
     }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == MainActivity2.RESULT_CODE){
+                        ed.setText(result.getData().getStringExtra("ppp"));
+                    }
+                }
+    });
+
+    // startActivityForResult(intent, 3);
+
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTIVITY_RESULT_CODE) {
+            // resultCode - либо ошибка, либо ОК.
+            Log.d("RRRR", Integer.toString(resultCode));
+            ed.setText(data.getStringExtra("ppp"));
+        }
+        if (requestCode == 3) {
+
+
+        }
+    }
+
+     */
 }
--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity2.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity2.java
@@ -2,22 +2,36 @@ package ru.samsung.itschool.mdev.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private TextView tv;
+    private Button btn3;
+    public static final int RESULT_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        btn3 = findViewById(R.id.button3);
         tv = findViewById(R.id.textView);
         Bundle bundle = getIntent().getExtras();
         if(bundle != null) {
             tv.setText(bundle.getString("ccc"));
         }
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("ppp","HELLO FROM ACTIVITY2");
+                setResult(RESULT_CODE,intent);
+                finish(); // завершение работы MainActivity2
+            }
+        });
     }
 }
